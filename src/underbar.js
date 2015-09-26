952745426654171ded8749abfceefafc673e9685
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-09-26 08:10:35 -0700

    uniq passes, reject still in progress

--- src/underbar.js
+++ src/underbar.js
@@ -97,12 +97,23 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
+    this.filter(collection, function () {
+      return !test.apply(this, arguments)
+    });
+
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var arr = []
+    this.each(array, function (val, index) {
+      if (_.indexOf(arr, val) === -1) {
+        arr.push(val);
+      }
+    })
+    return arr;
   };
 
 
